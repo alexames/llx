73a3826000eb95f8c97593d6b1a354d825e34400
Alexander Ames <Alexander.Ames@gmail.com>
2023-10-28 22:26:42 -0700

    Upgraded trace()

--- core/trace.lua
+++ core/trace.lua
@@ -1,5 +1,15 @@
-return function(...)
-  local source = debug.getinfo(2,'S').source 
-  local line = debug.getinfo(2,'l').currentline
-  print(string.format('%s:%s', source, line), ...)
+function trace(...)
+  local info = debug.getinfo(2, "Sln")
+  io.write(string.format('%s:%s', info.source:sub(2), info.currentline))
+  if info.name then
+    io.write(string.format(':%s', info.name))
+  end
+  if #{...} > 0 then
+    io.write(' ')
+  end
+  print(...)
 end
+
+return {
+  trace=trace,
+}
