d2e91ae4e118da4cda43e7c59648258ce39a67dd
Alexander Ames <Alexander.Ames@gmail.com>
2024-01-02 14:09:48 -0800

    Added a return type checker generator.

--- src/check_arguments.lua
+++ src/check_arguments.lua
@@ -3,6 +3,32 @@
 require 'llx/src/getclass'
 require 'llx/src/exceptions/invalid_argument_exception'
 
+local function check_argument(index, value, expected_type)
+  if expected_type == nil then
+    error(InvalidArgumentException(1, Table, getclass(value), 2))
+  else
+    local correct_type, exception = isinstance(value, expected_type)
+    if not correct_type then
+      if exception then
+        error(InvalidArgumentException(index, exception.what, 4))
+      else
+        error(InvalidArgumentTypeException(
+            index, expected_type, getclass(value), 4))
+      end
+    end
+  end
+end
+
+function check_returns(expected_types)
+  return function(...)
+    local return_values = {...}
+    for i=1, #expected_types do
+      check_argument(i, return_values[i], expected_types[i])
+    end
+    return ...
+  end
+end
+
 function check_arguments(expected_types)
   local index = 0
   repeat
@@ -10,21 +36,10 @@ function check_arguments(expected_types)
     local name, value = debug.getlocal(2, index)
     local expected_type = expected_types[name]
     if name then
-      if expected_type == nil then
-        error(InvalidArgumentException(1, Table, getclass(value), 1))
-      else
-        local correct_type, exception = isinstance(value, expected_type)
-        if not correct_type then
-          if exception then
-            error(InvalidArgumentException(index, exception.what, 3))
-          else
-            error(InvalidArgumentTypeException(
-                index, expected_type, getclass(value), 3))
-          end
-        end
-      end
+      check_argument(index, value, expected_type)
     end
   until name == nil
+  return check_return
 end
 
 return check_arguments
