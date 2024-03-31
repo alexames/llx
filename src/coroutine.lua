2f71d7f73d325c54981aeadddcafe7520a66dad8
Alexander Ames <Alexander.Ames@gmail.com>
2024-03-31 04:36:02 -0700

    Updated coroutine decorator to use scoped environment.

--- src/coroutine.lua
+++ src/coroutine.lua
@@ -2,18 +2,23 @@
 
 local class  = require 'llx/src/class' . class
 local Decorator = require 'llx/src/decorator' . Decorator
+local scoped_env = require 'llx/src/scoped_environment'
+
+local co_wrap = coroutine.wrap
+local unpack = table.unpack
+local _ENV <close> = scoped_env.create_environment()
 
 --- Wrap the member function using `coroutine.wrap`.
-local CoroutineDecorator = class 'CoroutineDecorator' : extends(Decorator) {
+WrapDecorator = class 'WrapDecorator' : extends(Decorator) {
   decorate = function(self, class_table, name, value)
     local function wrapped_function(...)
       local args = {...}
-      return coroutine.wrap(function() value(table.unpack(args)) end)
+      return co_wrap(function() value(unpack(args)) end)
     end
     return class_table, name, wrapped_function
   end,
 }
 
-return {
-  wrap=CoroutineDecorator(),
-}
+wrap = WrapDecorator()
+
+return _ENV
