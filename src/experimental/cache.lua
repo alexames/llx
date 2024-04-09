9713b2353a292135944e733ba82e85eeb7bdd01f
Alexander Ames <Alexander.Ames@gmail.com>
2024-04-09 09:15:18 -0700

    updated cache.lua to be a proper decorator.

--- src/experimental/cache.lua
+++ src/experimental/cache.lua
@@ -1,31 +1,51 @@
--- Copyright 2023 Alexander Ames <Alexander.Ames@gmail.com>
+-- Copyright 2024 Alexander Ames <Alexander.Ames@gmail.com>
 
-local function fakehash(arg)
-  return arg[1]
-end
+local class = require 'llx/src/class' . class
+local Decorator = require 'llx/src/decorator' . Decorator
+local environment = require 'llx/src/environment'
+local HashTable = require 'llx/src/hash_table' . HashTable
+local Tuple = require 'llx/src/tuple'. Tuple
 
-local function lru(count)
-  local cached_values = {}
+local _ENV, _M = environment.create_module_environment()
 
-  function cache_result(value, arg)
-    -- Assume one argument for now
-    for k, v in pairs(cached_values) do print(k, v) end
-    cached_values[fakehash(arg)] = value
-    return value
-  end
+local cache = class 'cache' : extends(Decorator) {
+  __init = function(self, params)
+    self.include_self = params and params.include_self
+  end,
 
-  return function(underlying_function)
-    return function(...)
-      local arg = {...}
-      -- Assume one argument for now,
-      -- switch fakehash to a real hash of the arguments at some point.
-      local result = cached_values[fakehash(arg)]
-      if not result then
-        result = cache_result({underlying_function(...)}, arg)
+  decorate = function(self, class_table, name, underlying_function)
+    local cache = HashTable{}
+    local include_self = self.include_self
+    local function wrapped_function(self, ...)
+      local key
+      if include_self then
+        key = Tuple{self, ...}
+      else
+        key = Tuple{...}
       end
-      return unpack(result)
+      local result = cache[key]
+      if result == nil then
+        result = {
+          value = underlying_function(self, ...),
+        }
+        cache[key] = result
+      end
+      return result.value
     end
-  end
-end
+    return class_table, name, wrapped_function
+  end,
+}
 
-return {lru=lru}
+-- FibTester = class 'FibTester' {
+--   ['fib' | cache{include_self=false}] = function(self, n)
+--     print(n)
+--     if n < 2 then return n
+--     else return self:fib(n-1) + self:fib(n-2)
+--     end
+--   end,
+-- }
+
+-- f = FibTester()
+-- print(f:fib(100))
+
+return _M
