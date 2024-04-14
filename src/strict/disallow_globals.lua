93f91b29a05209a673bdb6ab1e33b3cdea481ebe
Alexander Ames <Alexander.Ames@gmail.com>
2024-04-14 00:57:06 -0700

    Added scoped global lock functionality

--- src/strict/disallow_globals.lua
+++ src/strict/disallow_globals.lua
@@ -1,7 +1,32 @@
--- Copyright 2023 Alexander Ames <Alexander.Ames@gmail.com>
+-- Copyright 2024 Alexander Ames <Alexander.Ames@gmail.com>
 
-setmetatable(_G, {
-  __newindex = function(t, k, v)
-    error 'global writes disallowed'
-  end
-})
+local environment = require 'llx/src/environment'
+
+local _ENV, _M = environment.create_module_environment()
+
+local function disallow_writes(t, k, v)
+  local error_string = string.format(
+    'writes disallowed (attempted to write to [%s] = %s)', tostring(k),
+    tostring(v))
+  error(error_string, 2)
+end
+
+function lock_global_table()
+  local old_global_metatable = getmetatable(_G)
+  local new_global_metatable = old_global_metatable or {}
+
+  local old_global_newindex = new_global_metatable.__newindex
+  local new_global_newindex = disallow_writes
+
+  new_global_metatable.__newindex = new_global_newindex
+  setmetatable(_G, new_global_metatable)
+
+  return setmetatable({}, {
+    __close = function()
+      new_global_metatable.__newindex = old_global_newindex
+      setmetatable(_G, old_global_metatable)
+    end,
+  })
+end
+
+return _M
